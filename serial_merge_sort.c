ff455e897a1a8a1bb0d5f9efef5d00abf85bf8a1
Nalaka1693 <nalaka1693@gmai.com>
2017-08-26 18:40:22 +0530

    serial ok

--- serial_merge_sort.c
+++ serial_merge_sort.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define SIZE 8
 
@@ -9,65 +10,55 @@ int data_2[10] = {8, 5, 3, 9, 7, 6, 2, 1, 4, 0};
 int data_3[21] = {15, 8, 3, 4, 1, 3, 2, 7, 6, 10, 9, 1, 3, 8, 3, 9, 4, 2, 11, 6, 5};
 
 void merge_sort(int *, int);
-
-void split(int *, int, int, int *);
-
-void merge(int *, int, int, int, int *);
-
+void split(int *, int, int, int);
+void merge(int *, int, int, int, int);
 void print_arr(char *, int *, int, int);
 
 int main() {
     merge_sort(data_1, SIZE);
+    print_arr(NULL, data_1, 0, SIZE);
 
     return 0;
 }
 
 void merge_sort(int *arr, int size) {
-    int *copy = (int *) malloc(sizeof(int) * size);
-
-    split(arr, 0, size, copy);
-    print_arr(NULL, copy, 0, SIZE);
+    split(arr, 0, size, size);
 }
 
-void split(int *arr_1, int start, int end, int *arr_2) {
+void split(int *arr, int start, int end, int size) {
     if (end - start == 1) {
         return;
     }
 
     int mid = (start + end) / 2;
-//    print_arr("1_spl_s:", arr_1, start, mid);
-    split(arr_1, start, mid, arr_2);
-//    print_arr("1_spl_e:", arr_1, start, mid);
-//    print_arr("2_spl_s:", arr_1, mid, end);
-    split(arr_1, mid, end, arr_2);
-//    print_arr("2_spl_e:", arr_1, mid, end);
-    printf("s = %d m = %d e = %d\n", start, mid, end);
-    merge(arr_1, start, mid, end, arr_2);
+    split(arr, start, mid, size);
+    split(arr, mid, end, size);
+    merge(arr, start, mid, end, size);
 }
 
-void merge(int *arr_1, int start, int mid, int end, int *arr_2) {
-    print_arr("ms", arr_2, 0, SIZE);
-    if (mid - start <= 1) {
-        int i = start, j = 0;
-        for (; i < mid; i++) {
-            printf("a_s = %d a_m = %d\n", arr_1[i], arr_1[mid + j]);
-            if (arr_1[i] > arr_1[mid + j]) {
-                arr_2[i] = arr_1[mid + j];
-                arr_2[mid + j] = arr_1[i];
-            } else {
-                arr_2[i] = arr_1[i];
-                arr_2[mid + j] = arr_1[mid + j];
-            }
-            j++;
-        }
-    } else {
+void merge(int *arr, int start, int mid, int end, int size) {
+    int *copy = malloc(sizeof(int) * size);
+    memcpy(copy, arr, sizeof(int) * size);
 
+    int i = start, j = mid, k = start;
+
+    while (i < mid && j < end) {
+        if (copy[i] < copy[j]) {
+            arr[k++] = copy[i++];
+        } else {
+            arr[k++] = copy[j++];
+        }
     }
-    print_arr("me", arr_2, 0, SIZE);
+    
+    while (i < mid) {
+        arr[k++] = copy[i++];
+    }
+    while (j < end) {
+        arr[k++] = copy[j++];
+    }
 }
 
 void print_arr(char *msg, int *arr, int start, int end) {
-//    printf("s=%d, e=%d: ", start, end);
     if (msg) {
         printf("%s ", msg);
     }
--- test.c
+++ test.c
@@ -13,16 +13,14 @@ void print_arr(char *msg, int *arr, int start, int end) {
 }
 
 int *sort(int *arr_1, int *arr_2, int size_1, int size_2) {
-    int i = 0, j = 0, k = 0, n = 0;
+    int i = 0, j = 0, k = 0;
     int *result = (int *) malloc(sizeof(int) * (size_1 + size_2));
 
     while (i < size_1 && j < size_2) {
         if (arr_1[i] < arr_2[j]) {
-            result[k++] = arr_1[i];
-            i++;
+            result[k++] = arr_1[i++];
         } else {
-            result[k++] = arr_2[j];
-            j++;
+            result[k++] = arr_2[j++];
         }
     }
 
